0b94ac0e0004b264e85b903aa23836a36b81a9f8
Raϕ <72984363+BR31ZH8215@users.noreply.github.com>
2021-08-18 18:20:15 +0200

    The command now accepts cities with multiple words + paging

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ client.remove_command('help')
 
 
 @client.command()
-async def current(ctx, arg):
-    complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+ arg +"&appid=6e2d2db27e5a42fa384a698d859fc686"
+async def current(ctx,*,args):
+    complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+ args +"&appid=6e2d2db27e5a42fa384a698d859fc686"
     api_link=requests.get(complete_api_link)
     api_data=api_link.json()
 
@@ -20,11 +20,12 @@ async def current(ctx, arg):
 
     else:
         temperature=((api_data['main']['temp'])-273.15)
-        embed=discord.Embed(title=f'Here is {arg}\' weather right now', description='Reminder that it\'s not always accurate, pls no bully me', color=0xce2029)\
+        windspeed=((api_data['wind']['speed'])*3.6)
+        embed=discord.Embed(title=f'Here is {args}\'s weather right now', description='Reminder that it\'s not always accurate, pls no bully me', color=0xce2029)\
             .add_field(name='Weather description:',value=f"{api_data['weather'][0]['description']}", inline=False)\
-            .add_field(name='Average temperature', value=f"{int(temperature):,.2f} °C",inline=False)\
+            .add_field(name='Average temperature', value=f"{float(temperature):,.2f} °C",inline=False)\
             .add_field(name='Humidity',value=f"{api_data['main']['humidity']}%", inline=False)\
-            .add_field(name='Wind speed', value=f"{api_data['wind']['speed']} km/h", inline=False)
+            .add_field(name='Wind speed', value=f"{windspeed:,.2f} m/s", inline=False)
 
     embed.set_footer(text="Weather Bot | Developed by me")
     embed.timestamp=datetime.now()
