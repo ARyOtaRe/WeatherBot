9e2887b7898bb45751911bf62e800906a4522e16
Raϕ <72984363+BR31ZH8215@users.noreply.github.com>
2021-09-01 14:21:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ async def current(ctx,*,args):
     complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+ args +"&appid=6e2d2db27e5a42fa384a698d859fc686"
     api_link=requests.get(complete_api_link)
     api_data=api_link.json()
+    print(complete_api_link)
 
     if api_data['cod']=='404':
         embed=discord.Embed(title='The city you\'re trying to get the weather for does not exist!', description='Please check if you made any typos, and then ask about weather in a bigger city around yours', color=0xce2029)
@@ -21,8 +22,8 @@ async def current(ctx,*,args):
         temperature=((api_data['main']['temp'])-273.15)
         windspeed=((api_data['wind']['speed'])*3.6)
         city = f'{args}\'' if args[-1]=='s' else f'{args}\'s'
-        embed=discord.Embed(title=f'Here is {city} weather right now', description='Reminder that it\'s not always accurate, pls no bully me', color=0xce2029)\
-            .add_field(name='Weather description:',value=f"{api_data['weather'][0]['description']}", inline=False)\
+        embed=discord.Embed(title=f'Here is {city.capitalize()} weather right now', description='Reminder that it\'s not always accurate, pls no bully me', color=0xce2029)\
+            .add_field(name='Weather description:',value=f"{api_data['weather'][0]['description']}".capitalize(), inline=False)\
             .add_field(name='Average temperature', value=f"{float(temperature):,.2f} °C",inline=False)\
             .add_field(name='Humidity',value=f"{api_data['main']['humidity']}%", inline=False)\
             .add_field(name='Wind speed', value=f"{windspeed:,.2f} m/s", inline=False)
