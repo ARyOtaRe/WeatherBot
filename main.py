681a1463c975e8ceedf59c86417b77901ee24c6c
Raϕ <72984363+ARyOtaRe@users.noreply.github.com>
2021-12-02 23:37:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import json
 
 
 
-with open(os.path.join("C:\\Users\\ARyOtaRe\\Documents\\GitHub\\WeatherBot",'tokens.json'),'r') as token_file:
-    Tokens=json.loads(token_file.read())
 
 client=commands.Bot(command_prefix=commands.when_mentioned_or('WeatherBot:'))
+
+with open(os.path.join("C:\\Users\\ARyOtaRe\\Documents\\GitHub\\WeatherBot",'tokens.json'),'r') as token_file:
+    Tokens=json.loads(token_file.read())
 
 @client.event
 async def on_message(message):
@@ -26,11 +27,16 @@ async def on_ready():
     print("yahoo")
 
 @client.command(description='Shows the weather information of the city of your choice!',aliases=["crt","now"])
-async def current(ctx,*,args):
-    complete_api_link=f"https://api.openweathermap.org/data/2.5/weather?q={args}&appid={Tokens["openweathermap_tokens"]["appid"]}"
-    api_link=requests.get(complete_api_link)
-    api_data=api_link.json()
-    print(complete_api_link)
+async def current(ctx,*args):
+    try:
+        complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+f"{args}" +"&appid="+ f"{Tokens['openweathermap_tokens']['appid']}"
+        cmoplete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+args+"&appid=6e2d2db27e5a42fa384a698d859fc686"
+        api_link=requests.get(cmoplete_api_link)
+        api_data=api_link.json()
+        print(complete_api_link)
+        print(cmoplete_api_link)
+    except:
+        print(Exception)
 
     if api_data['cod']=='404':
         embed=discord.Embed(title='The city you\'re trying to get the weather from does not exist!', description='Please check if you\'ve made any typo, or get the weather from the closest city :)', color=0xce2029)
@@ -40,10 +46,10 @@ async def current(ctx,*,args):
         city = f'{args}\'' if args[-1]=='s' else f'{args}\'s'
         fahr=9.0/5.0 * temperature + 32
         embed=discord.Embed(title=f'{city.capitalize()} right now', description='It\'s not always the case, keep that in mind', color=0xce2029)\
-            .add_field(name='Description :',value=f"{api_data['weather'][0]['description']}".capitalize(), inline=False)\
-            .add_field(name='Average temperature :', value=f"{float(temperature):,.2f} °C/{fahr:,.0f} °F ",inline=False)\
-            .add_field(name='Average humidity',value=f"{api_data['main']['humidity']}%", inline=False)\
-            .add_field(name='Wind speed', value=f"{windspeed:,.2f} m/s", inline=False)
+        .add_field(name='Description :',value=f"{api_data['weather'][0]['description']}".capitalize(), inline=False)\
+        .add_field(name='Average temperature :', value=f"{float(temperature):,.2f} °C/{fahr:,.0f} °F ",inline=False)\
+        .add_field(name='Average humidity',value=f"{api_data['main']['humidity']}%", inline=False)\
+        .add_field(name='Wind speed', value=f"{windspeed:,.2f} m/s", inline=False)
 
     embed.set_footer(text="WeatherBot | Developed by your fav dev")
     embed.timestamp=datetime.now()
